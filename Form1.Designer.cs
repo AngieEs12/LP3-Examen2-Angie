0d5e69f2f4b38f089dda842a5acdd7f3fca66785
AngieEs12 <90420690+AngieEs12@users.noreply.github.com>
2021-11-12 20:54:34 -0600

    labellogin

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -37,9 +37,9 @@ namespace LP3_Examen2_Angie
             this.label1.AutoSize = true;
             this.label1.Location = new System.Drawing.Point(156, 91);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.Size = new System.Drawing.Size(33, 13);
             this.label1.TabIndex = 0;
-            this.label1.Text = "label1";
+            this.label1.Text = "Login";
             // 
             // Form1
             // 
